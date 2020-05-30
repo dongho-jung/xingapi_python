5bb1dbe068f303a3d8225faf5d36e1ed6a46a383
0xF4D3C0D3 <dongho971220@gmail.com>
2020-05-31 00:01:03 +0900

    make get_secrets more robust

--- src/util/get_credential.py
+++ src/util/get_credential.py
@@ -1,13 +1,31 @@
+import util.logger
+
 import json
 import os
+
+logger = util.logger.get()
 
 
 def get_secrets(path: str = None):
     """
     Args:
         path (str) <{workingdir/.secrets.json}>: path in which a secret file exists
+    Note:
+        if there's no secret file, it searches environment variable
     Returns (dict): secret context
     """
-    path = path or os.path.join(os.getcwd(), ".secrets.json")
-    with open(path) as f:
-        return json.load(f)
+    try:
+        path = path or os.path.join(os.getcwd(), ".secrets.json")
+        with open(path) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        try:
+            return {
+                "ID": os.environ["XING_ID"],
+                "PW": os.environ["XING_PW"],
+                "DEMO_PW": os.environ["XING_DEMO_PW"],
+                "CERT_PW": os.environ["XING_CERT_PW"],
+            }
+        except KeyError as e:
+            logger.fatal("can't find secret information", e)
+            raise AssertionError
