e232b54c23440a752e684089c59184a4f4726f14
0xF4D3C0D3 <dongho971220@gmail.com>
2020-05-31 18:29:06 +0900

    add sanity check for parameter res to avoid stuck by PumpMessages

--- src/com/XAQuery.py
+++ src/com/XAQuery.py
@@ -2,6 +2,9 @@ import os
 import win32com
 
 from util.deco import final_post_quit
+import util.logger
+
+logger = util.logger.get()
 
 
 class XAQueryEventHandler:
@@ -11,12 +14,16 @@ class XAQueryEventHandler:
 
     @final_post_quit
     def OnReceiveData(self, code):
-        pass
+        logger.debug(f"OnReceiveData: code={code}")
 
 
 def get_xaquery_event_proxy(res, res_path="C:/eBEST/xingAPI/Res/"):
+    res_file_name = os.path.join(res_path, res + ".res")
+    if not os.path.isfile(res_file_name):
+        logger.fatal(f"can't find {res_file_name}")
+        raise FileNotFoundError
     event_proxy = win32com.client.DispatchWithEvents(
         "XA_DataSet.XAQuery", XAQueryEventHandler
     )
-    event_proxy.ResFileName = os.path.join(res_path, res + ".res")
+    event_proxy.ResFileName = res_file_name
     return event_proxy
